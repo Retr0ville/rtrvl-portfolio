235d5922512ec3312468348ce60a8964bb8c8057
Retr0ville <xclusivesam14@gmail.com>
2022-06-27 16:42:35 +0100

    sw

--- src/hooks/useFocusHook.ts
+++ src/hooks/useFocusHook.ts
@@ -1,12 +1,16 @@
 import { useState } from "react";
 
-const useFocus = ()=> {
-    const [isFocused, setIsFocused] = useState({});
-    const toggleFocus =(projectName: string)=> {
-        setIsFocused({[projectName] : true})
+const useFocus = () => {
+  const [isFocused, setIsFocused] = useState<{ [x: string]: string,  }>({});
+  const toggleFocus = (caller : {[x:string]: string}) => {
+    if (isFocused[Object.keys(caller)[0]]) {
+      setIsFocused({});
+      return
     }
+    setIsFocused({...caller });
+  };
 
-    return [ isFocused, toggleFocus ]
-}
+  return {isFocused, toggleFocus};
+};
 
-export default useFocus
+export default useFocus;
--- src/utils/skills.tsx
+++ src/utils/skills.tsx
@@ -1,6 +1,118 @@
-export type skillsType= {
-  string[]
+export interface SkillsType {
+  frontend: Frontend;
+  backend: Backend;
+  devsAndSrc: DevsAndSrc;
+  extra: Extra;
 }
-export const skills : skillsType = [
 
-]
+interface Extra {
+  Blender: string;
+  zBrush: string;
+  Wordpress: string;
+  'Chemical Engineering': string;
+}
+
+interface DevsAndSrc {
+  Docker: string;
+  TravisCI: string;
+  CircleCI: string;
+  AWSElasticBeanstalk: string;
+  Git: string;
+}
+
+interface Backend {
+  db: Db;
+  frameworks: Frameworks;
+}
+
+interface Frameworks {
+  Node: string;
+  Express: string;
+  Mongoose: string;
+}
+
+interface Db {
+  MongoDB: string;
+  Firebase: string;
+  GraphQL: string;
+  Cloudinary: string;
+}
+
+interface Frontend {
+  webFrames: WebFrames;
+  pLangs: PLangs;
+  styleFrames: StyleFrames;
+}
+
+interface StyleFrames {
+  Bootstrap: string;
+  Tailwind: string;
+  'Styled-components': string;
+  ChakraUI: string;
+  SemanticUI: string;
+  'React-Spring': string;
+}
+
+interface PLangs {
+  Typescript: string;
+  Javascript: string;
+  HTML5: string;
+  CSS3: string;
+}
+
+interface WebFrames {
+  React: string;
+  Next: string;
+  EJS: string;
+  Gatsby: string;
+}
+export const skills : SkillsType = {
+  frontend: {
+    webFrames:{
+      React:'re',
+      Next:'ne',
+      EJS:'ej',
+      Gatsby:'ga',
+    },
+    pLangs:{
+      Typescript:'ty',
+      Javascript:'ja',
+      HTML5:'ht',
+      CSS3:'cs',
+    },
+    styleFrames:{
+      Bootstrap:'bo',
+      Tailwind:'ta',
+      'Styled-components':'st',
+      ChakraUI:'ch',
+      SemanticUI:'se',
+      'React-Spring':'re',
+    },
+  },
+  backend:{
+    db:{
+      MongoDB:'',
+      Firebase:'',
+      GraphQL:'',
+      Cloudinary:'',
+    },
+    frameworks:{
+      Node:'',
+      Express:'',
+      Mongoose:'',
+    },
+  },
+  devsAndSrc:{
+    Docker:'',
+    TravisCI:'',
+    CircleCI:'',
+    AWSElasticBeanstalk:'',
+    Git:''
+  },
+  extra:{
+    Blender:'',
+    zBrush:'',
+    Wordpress:'',
+    'Chemical Engineering':''
+  }
+}
