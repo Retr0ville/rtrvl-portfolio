d6405894c198fda93f787a4d93b5c1bdec28f91a
Retr0ville <xclusivesam14@gmail.com>
2022-07-06 14:03:49 +0100

    format useFocus hook

--- src/hooks/useFocusHook.ts
+++ src/hooks/useFocusHook.ts
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 const useFocus = () => {
-  const [isFocused, setIsFocused] = useState<{ [x: string]: string,  }>({});
-  const toggleFocus = (caller : {[x:string]: string}) => {
+  const [isFocused, setIsFocused] = useState<{ [x: string]: string }>({});
+  const toggleFocus = (caller: { [x: string]: string }) => {
     if (isFocused[Object.keys(caller)[0]]) {
       setIsFocused({});
-      return
+      return;
     }
-    setIsFocused({...caller });
+    setIsFocused({ ...caller });
   };
 
-  return {isFocused, toggleFocus};
+  return { isFocused, toggleFocus };
 };
 
 export default useFocus;
